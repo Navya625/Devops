f77fd3d4991125f56028ca3e3d960c29f66e7937
Navya625 <163966069+Navya625@users.noreply.github.com>
2025-03-19 09:53:24 +0000

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,12 +3,5 @@ class hello
 {
 public static void main(String args[ ])
 {
-Scanner sc=new Scanner(System.in);
-int a=sc.nextInt();
-int b=sc.nextInt();
-System.out.println(+(a+b)+ "Sum");
-System.out.println(+(a-b)+"Sub");
-System.out.println(+(a*b)+"mul");
-System.out.println(+(a/b )+"div");
-System.out.println(+(a%b)+"mod");
+Syste.out.println("Hello world");
 }}
