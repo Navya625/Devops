86b24dfc685e24c1636d061db4c99f5d3b966f0d
Navya625 <163966069+Navya625@users.noreply.github.com>
2025-03-19 09:44:14 +0000

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class hello
+{
+public static void main(String args[ ])
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+System.out.println(+(a+b)+ "Sum");
+System.out.println(+(a-b)+"Sub");
+System.out.println(+(a*b)+"mul");
+System.out.println(+(a/b )+"div");
+System.out.println(+(a%b)+"mod");
+}}
